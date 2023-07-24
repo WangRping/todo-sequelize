22b36ca30202083a2d1976c6e49fafe70a9d0f5d
WangRping <r17369@gmail.com>
2023-07-24 21:51:54 +0800

    安裝SQL相關套件 並且測試註冊功能是否可以寫入資料庫

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 
 const PORT = 3000
 const app = express()
@@ -13,7 +16,29 @@ app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('hello world')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {
