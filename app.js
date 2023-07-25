d16f97bf710ac685ea8166b0c151af12ab451843
WangRping <r17369@gmail.com>
2023-07-25 11:49:51 +0800

    add user authentication in todo and home routers

--- app.js
+++ app.js
@@ -2,28 +2,23 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
 const session = require('express-session')
-const passport = require('passport')
 const usePassport = require('./config/passport')
 const router = require('./routes/index')
 const db = require('./models')
-const Todo = db.Todo
-const User = db.User
+
 
 const PORT = 3000
 const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
-usePassport(app)
-
 app.use(session({
   secret: 'ThisIsMySecret',
   resave: false,
   saveUninitialized: true
 }))
+usePassport(app)
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
